7c058ea212fb289b186478dc36049faac5da24ad
db <zoudb@gzyct.com>
2016-05-24 22:53:04 +0800

    add file test

--- app/src/main/java/android/baodian/com/biaodiangithub/MainActivity.java
+++ app/src/main/java/android/baodian/com/biaodiangithub/MainActivity.java
@@ -1,13 +1,18 @@
 package android.baodian.com.biaodiangithub;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
